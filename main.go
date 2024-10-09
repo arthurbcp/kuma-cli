77d553e710927b02a377d7263ec1686ee013d68b
Arthur Polimeni <arthurpolimeni17@gmail.com>
2024-10-08 21:50:16 -0300

    function package with docs

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "github.com/arthurbcp/kuma-cli/cmd"
 
 func main() {
+	test :
+	map[string]interface{} {
+		"items": map[string]interface{} {
+	   		"subItems": []string { "item1", "item2", "item3"}
+		} 
+	}
 	cmd.Execute()
 }
